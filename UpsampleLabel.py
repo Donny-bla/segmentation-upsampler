0cfdcba9abaa2a96be8a6a9433243ddd74e4a7ba
Donny-bla <120373788+Donny-bla@users.noreply.github.com>
2024-02-22 12:43:37 +0000

    enable cropping and fix axis error

--- UpsampleLabel.py
+++ UpsampleLabel.py
@@ -4,12 +4,29 @@ from Extractor import IsosurfaceExtractor
 from Voxelizer import MeshVoxelizer
 import numpy as np
 
-preprocessor = MeshPreprocessor(originalMatrix, sigma, targetVolume)
-smoothedMatrix, isovalue = preprocessor.meshPreprocessing()
-print(isovalue)
-isosurfaceExtractor = IsosurfaceExtractor(smoothedMatrix, isovalue)
+padWidth = 5
+paddedMatrix = np.pad(originalMatrix, pad_width = padWidth, mode='constant', constant_values=0)
+
+preprocessor = MeshPreprocessor(paddedMatrix, sigma, targetVolume)
+smoothedMatrix, isovalue, croppedMatrix, bounds  = preprocessor.meshPreprocessing() #
+croppedMatrix = np.ascontiguousarray(croppedMatrix)
+print(croppedMatrix.shape)
+print(bounds)
+isosurfaceExtractor = IsosurfaceExtractor(croppedMatrix, isovalue)
 faces, nodes, polyData = isosurfaceExtractor.extractIsosurface()
 
-gx, gy, gz = np.shape(originalMatrix)
-voxelizer = MeshVoxelizer(polyData, gx, gy, gz, scale)
-newMatrix = voxelizer.voxeliseMesh()
+
+gx, gy, gz = np.shape(paddedMatrix)
+background = np.zeros((int(gx / scale),
+                        int(gy / scale),
+                        int(gz / scale)), dtype=np.uint8)
+label = 1
+x, y, z = np.shape(croppedMatrix)
+voxelizer = MeshVoxelizer(polyData, x, y, z, scale, background, bounds, label)
+paddedNewMatrix = voxelizer.voxeliseMesh()
+
+unpad = int(padWidth/scale)
+newMatrix = paddedNewMatrix[unpad:-unpad, unpad:-unpad, unpad:-unpad]
+newMatrix = np.ascontiguousarray(newMatrix)
+print(newMatrix.shape)
+
