bd20c68ebb7e08c57d627d018908e5c4ab82fbe3
Donny-bla <120373788+Donny-bla@users.noreply.github.com>
2024-02-22 12:37:52 +0000

    Update SimpleUsage.m
    Include usage for multilabels

--- SimpleUsage.m
+++ SimpleUsage.m
@@ -5,13 +5,18 @@ clear all;
 % Low resolution.
 N = 60;
 radius = round(N/3);
-originalMatrix = makeShapes("Complex", [radius], [N,N,N], [0, 0, 0]);
+%originalMatrix = makeShapes("Complex", [radius], [N,N,80], [0, 0, 0]);
+originalMatrix = makeShapes("MultiLabel", [radius], [N,N,80], [0, 0, 0]);
 %originalMatrix = makeShapes("BallHole",[radius, floor(radius/3)], [N,N,N],[0,0,0]);
 %%
 % Set grid spacing.
-dx = 0.25;
-sigma = 0.68;
-newMatrix = pyUpsampleLabel(originalMatrix, dx, sigma, 0);
+dx = 0.29;
+sigma = 0.68; Volume = 0;
+%sigma = 0.8; Volume = 0.22;
+%newMatrix = pyUpsampleLabel(originalMatrix, dx, sigma, Volume);
+pyenv;
+newMatrix = pyrunfile("UpsampleMultiLabels.py","newMatrix", multiLabelMatrix = py.numpy.array(originalMatrix), sigma = sigma, targetVolume = Volume, scale = dx);
+
 % while sum(newMatrix,"all") == 0
 %     newMatrix = UpsampleLabel(originalMatrix, dx, sigma);
 % end
@@ -20,11 +25,11 @@ newMatrix = pyUpsampleLabel(originalMatrix, dx, sigma, 0);
 Nref = floor(N / dx);
 fac = 1 / dx;
 radiusRef = radius / dx;
-referenceMatrix = makeShapes("Complex", [radiusRef], [Nref,Nref,Nref], [0, 0, 0]);
+%referenceMatrix = makeShapes("Complex", [radiusRef], [207,207,276], [0, 0, 0]);
+referenceMatrix = makeShapes("MultiLabel", [radiusRef], [Nref,Nref,floor(80/0.29)], [0, 0, 0]);
 %referenceMatrix = makeShapes("BallHole",[radiusRef, floor(radiusRef/3)], [Nref,Nref,Nref],[0,0,0]);
-
 %% Plot
-
+newMatrix = double(newMatrix);
 figure;
 subplot(1, 5, 1);
 imagesc(newMatrix(:, :, round(end/2)));
@@ -54,7 +59,7 @@ title('Difference Image - Y-axis')
 %%
 DoC = DegreeOfComplexity(originalMatrix);
 DifferenceMatrix = abs(referenceMatrix-newMatrix);
-voxelPlot(DifferenceMatrix)
+voxelPlot(single(DifferenceMatrix>0))
 Diff = sum(DifferenceMatrix,"all");
 DoCgrade = Diff * DoC ^ 3;
 fprintf('Grade by Degree of Complexity: %d\n',DoCgrade);
