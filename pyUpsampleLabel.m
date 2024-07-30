e9e2cd1615d308a8353d419a99205e470c77be3f
Donny-bla <120373788+Donny-bla@users.noreply.github.com>
2024-07-30 11:44:11 +0100

    Delete pyUpsampleLabel.m

--- pyUpsampleLabel.m
+++
@@ -1,9 +0,0 @@
-function UpsampledMatrix = pyUpsampleLabel(originalMatrix, dx, sigma, varargin)
-if nargin<4
-    Volume = -0.17;
-else
-    Volume = varargin{1};
-end
-
-newM = pyrunfile("UpsampleLabel.py","newMatrix", originalMatrix = py.numpy.array(originalMatrix), sigma = sigma, targetVolume = Volume, scale = dx);
-UpsampledMatrix = single(newM);
