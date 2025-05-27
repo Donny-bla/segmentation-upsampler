019349445bd9b4edc047364ef73fc16900969816
Donny-bla <zcemll0@ucl.ac.uk>
2025-05-27 15:54:18 +0100

    update testing

--- src/test/test_upsample_multilabels.py
+++ src/test/test_upsample_multilabels.py
@@ -1,13 +1,13 @@
 import numpy as np
 from SegmentationUpsampler.UpsampleMultiLabels import upsample
+import os
 
-image = np.load("data/multilabelTestShape.npy")
+base_path = os.path.dirname(__file__)
+data_dir = os.path.join(base_path, "../data/")
+image = np.load(os.path.join(data_dir, "multilabelTestShape.npy"))
 
 def test_upsample_nbtrue_fillfalse():
-    #base_path = os.path.dirname(__file__)
-    #data_dir = os.path.join(base_path, "../data/")
-    #image = np.load(os.path.join(data_dir, "multilabelTestShape.npy"))
-    expected = np.load("data/NBTrueFillGapsFalse.npy")
+    expected = np.load(os.path.join(data_dir, "NBTrueFillGapsFalse.npy"))
     scale = [0.5, 0.5, 0.5]
     sigma = 0.6
     iso = 0.4
@@ -19,10 +19,7 @@ def test_upsample_nbtrue_fillfalse():
     np.testing.assert_array_equal(result, expected)
 
 def test_upsample_nbfalse_fillfalse():
-    #base_path = os.path.dirname(__file__)
-    #data_dir = os.path.join(base_path, "../data/")
-    #image = np.load(os.path.join(data_dir, "multilabelTestShape.npy"))
-    expected = np.load("data/NBFalseFillGapsFalse.npy")
+    expected = np.load(os.path.join(data_dir, "NBFalseFillGapsFalse.npy"))
     scale = [0.5, 0.5, 0.5]
     sigma = 0.6
     iso = 0.4
@@ -34,10 +31,7 @@ def test_upsample_nbfalse_fillfalse():
     np.testing.assert_array_equal(result, expected)
 
 def test_upsample_nbfalse_filltrue():
-    #base_path = os.path.dirname(__file__)
-    #data_dir = os.path.join(base_path, "../data/")
-    #image = np.load(os.path.join(data_dir, "multilabelTestShape.npy"))
-    expected = np.load("data/NBFalseFillGapsTrue.npy")
+    expected = np.load(os.path.join(data_dir, "NBFalseFillGapsTrue.npy"))
     scale = [0.5, 0.5, 0.5]
     sigma = 0.6
     iso = 0.4
@@ -49,10 +43,7 @@ def test_upsample_nbfalse_filltrue():
     np.testing.assert_array_equal(result, expected)
 
 def test_upsample_nbtrue_filltrue():
-    #base_path = os.path.dirname(__file__)
-    #data_dir = os.path.join(base_path, "../data/")
-    #image = np.load(os.path.join(data_dir, "multilabelTestShape.npy"))
-    expected = np.load("data/NBTrueFillGapsTrue.npy")
+    expected = np.load(os.path.join(data_dir, "NBTrueFillGapsTrue.npy"))
     scale = [0.5, 0.5, 0.5]
     sigma = 0.6
     iso = 0.4
